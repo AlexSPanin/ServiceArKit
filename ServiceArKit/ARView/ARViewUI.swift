79e42c34cc8064b1defd942c92e63a258d8a01de
Alexander Panin <panin1970@gmail.com>
2024-10-24 08:54:14 +0300

    add file

---
+++ ServiceArKit/ARView/ARViewUI.swift
@@ -0,0 +1,18 @@
+//
+//  ARViewUI.swift
+//  ServiceArKit
+//
+//  Created by Александр Панин on 23.10.2024.
+//
+
+import SwiftUI
+
+struct ARViewUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ARViewUI()
+}
