77d36e9987fccc78daaa7780423b7f4dba60804d
Alexander Panin <panin1970@gmail.com>
2024-11-20 21:58:14 +0300

    add ButtonView File

---
+++ ServiceArKit/Views/ButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonView.swift
+//  ServiceArKit
+//
+//  Created by Александр Панин on 20.11.2024.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonView()
+}
